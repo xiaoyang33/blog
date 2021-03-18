a8a7d28e4f6f218ba87a42f31623be725e448400
yang wenlong <yangwenlong@maankeji.com>
2021-03-18 18:15:24 +0800

    富文本处理

--- blog-backstage/src/router/index.js
+++ blog-backstage/src/router/index.js
@@ -4,25 +4,52 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
-    {
-      path:'/',
-      redirect:'Login'
-    },
-    {
-      path:'/login',
-      name:'Login',
-      component:()=>import('../views/user/Login.vue')
-    },
-    {
-      path:'/index',
-      name:'Index',
-      component:()=>import('../views/mainPages/index.vue')
-    }
+  {
+    path: '/',
+    redirect: 'Login'
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/views/Login.vue')
+  },
+  {
+    path: '/main',
+    name: 'Main',
+    redirect: '/main/index',
+    component: () => import('@/views/Main.vue'),
+    children: [
+      {
+        name: 'Index',
+        path: '/index',
+        component: () => import('@/views/pages/Index.vue')
+      },
+      {
+        name: 'WritePage',
+        path: '/writepage',
+        component: () => import('@/views/pages/WritePage.vue')
+      },
+      {
+        path:'/aaa',
+        component: () => import('@/views/pages/aaa.vue'),
+        children:[
+          {
+            path:'/bbb',
+            component: () => import('@/views/pages/bbb.vue'),
+          },
+          {
+            path:'/ccc',
+            component: () => import('@/views/pages/ccc.vue'),
+          }
+        ]
+      }
+    ]
+  }
 ]
 
 const router = new VueRouter({
   routes,
-  mode:'history'
+  mode: 'history'
 })
 
 export default router
--- blog-backstage/vue.config.js
+++ blog-backstage/vue.config.js
@@ -1,6 +1,11 @@
-
+const webpack = require('webpack')
 module.exports = {
-
+    // 解决Cli3 quill-image-resize-module的报错
+    chainWebpack: config => {
+      config.plugin('provide').use(webpack.ProvidePlugin, [{
+          'window.Quill': 'quill'
+      }]);
+  },
     configureWebpack:{
       // 配置路径文件名
       resolve:{
