fc6941f2c8e42a53e4d20f090d904e7a48479e1c
yangwenlong <81355146@qq.com>
2021-03-18 23:01:58 +0800

    枚举

--- blog-server/mongoose/backStage/Operations.js
+++ blog-server/mongoose/backStage/Operations.js
@@ -3,11 +3,20 @@ const MyMongo = require('../mongoose')
 
 const {mongoose , db} = MyMongo()
 
+/* 
+    articleTitle:文章标题
+    articleContent:文章内容
+    tag:标签
+    thumbnail:缩略图
+    classIfy:分类
+*/
+
 let articleScheam = mongoose.Schema({
     articleTitle:String,
     articleContent:String,
     tag:String,
     thumbnail:String,
+    classIfy:String,
     createDate:{
         type:String,
         default:String(new Date().getTime())
