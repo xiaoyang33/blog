f39186c9c72fab1b89758b468a8e3514681f2c99
yang wenlong <yangwenlong@maankeji.com>
2021-03-24 18:01:46 +0800

    c侧边栏模块

--- my-blog/src/views/Home.vue
+++ my-blog/src/views/Home.vue
@@ -1,17 +1,42 @@
 <template>
-  <div style="width:80%">
-      <page-module-1 v-for="i in 10" :index="i"></page-module-1>
-  </div>
+    <div class="main-container">
+        <div class="article">
+            <article-module-1
+                v-for="i in 10"
+                :index="i"
+                :key="i"
+            ></article-module-1>
+        </div>
+        <div class="aside">
+            <aside-module-1>
+              <template v-slot:title>标题</template>
+            </aside-module-1>
+        </div>
+    </div>
 </template>
 
 <script>
-import PageModule1 from '../components/my-modules/PageModule1.vue'
+import ArticleModule1 from "@/components/my-modules/ArticleModule1.vue";
+import AsideModule1 from "@/components/my-modules/AsideModule1.vue";
 export default {
-    components: { PageModule1 },
-
-}
+    components: {
+        ArticleModule1,
+        AsideModule1,
+    },
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.main-container {
+    display: flex;
+    .article {
+        margin-right: 30px;
+        flex-shrink: 0;
+        width: 70%;
+    }
+    .aside {
+        // flex: 1;
+        width: 30%;
+    }
+}
 </style>
