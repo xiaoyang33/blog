eeaf91a67fd3da3efd8a9f6a2efb76d4463218bf
yang wenlong <yangwenlong@maankeji.com>
2021-03-31 18:10:41 +0800

    周三

---
+++ my-blog/src/components/common/ClickBubbles.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="bubbles" ref="bubbles">
+      dasdasd
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        x:Number,
+        y:Number
+    },
+    watch:{
+        x(n){
+            console.log(n);
+            this.showPosition()
+        }
+    },
+    methods:{
+        showPosition(){
+            let el = this.$refs.bubbles
+            // console.log(el)
+            el.style.left = this.x +'px'
+            el.style.top = this.y + 'px'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.bubbles{
+    position: absolute;
+    top: 1px ;
+    left: 1px;
+}
+</style>
